b5e3fcfadd0f4e9f669a05791bacce5074698ef0
anishka-patel <anishka.vpatel@gmail.com>
2026-06-12 16:13:11 +0530

    Fix --allowerasing

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 remove code --allowerasing
+dnf5 remove code
 dnf5 swap nano-default-editor vim-default-editor --allowerasing
 dnf5 install -y kitty neovim emacs-pgtk
 
